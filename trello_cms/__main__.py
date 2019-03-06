9fb6ab94d43e66c920ec421ed9939288515f00cc
Jaco du Plessis <jaco@jacoduplessis.co.za>
2019-03-06 07:54:06 +0200

    Build list and single views per card

--- trello_cms/__main__.py
+++ trello_cms/__main__.py
@@ -25,13 +25,14 @@ if __name__ == '__main__':
 
     boards = {}
     if args.local:
-        for board_id in config.get('boards', {}).keys():
+        for board_id, board_config in config.get('boards', {}).items():
 
             path = data_dir.joinpath(f'{board_id}.json')
             if not path.exists():
                 print(f"[warning] Board {board_id} not found in {data_dir}, skipping.")
                 continue
             board_data = json.loads(path.read_bytes())
+            board_data['config'] = board_config
             boards[board_id] = TBoard(board_data)
 
     else:
@@ -47,3 +48,32 @@ if __name__ == '__main__':
 
     html = index.render(boards=boards)
     dist.joinpath('index.html').write_text(html)
+
+    for board_id, board_config in config.get('boards', {}).items():
+
+        template_list = board_config.get('template_list')
+        template_single = board_config.get('template_single')
+
+        slug = board_config.get('slug')
+
+        if slug is None:
+            print(f"Not processing board {board_id}: slug is None")
+            continue
+
+        board_dir = dist.joinpath(slug)
+        board_dir.mkdir(exist_ok=True)
+
+        if template_list:
+            path = board_dir.joinpath('index.html')
+            html = env.get_template(template_list).render()
+            path.write_text(html)
+
+        if template_single:
+
+            template = env.get_template(template_single)
+
+            for card in boards[board_id].cards:
+
+                html = template.render(card=card)
+                path = board_dir.joinpath(f'{card.id}.html')
+                path.write_text(html)
